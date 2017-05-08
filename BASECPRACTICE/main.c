9238cf14310772dc4c890c29c62e82b3c3ed4f59
shiios <s292826452@163.com>
2017-05-08 11:21:49 +0800

    Initial Commit

---
+++ BASECPRACTICE/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BASECPRACTICE
+//
+//  Created by kalian on 2017/5/8.
+//  Copyright © 2017年 kalian. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
